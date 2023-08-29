bba1008bd01ad073f041dd351e4d806b1ea36215
Vivek Yanamadula <vivekyanamadula@gmail.com>
2023-08-29 02:17:27 -0400

    Adding useful utils to templates

--- template.cpp
+++ template.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 #define pb push_back
 #define mp make_pair
-#define mpi pair<int, int>
+#define pii pair<int, int>
 #define a first
 #define b second
 #define vi vector<int>
@@ -14,12 +14,20 @@ using namespace std;
 #define imax INT_MAX
 #define imin INT_MIN
 #define len(x) (int((x).size()))
-#define str std::string
+#define si set<int>;
+#define mii map<int, int>;
+
+#define printv(a) {for(auto u:a) cout<<u<<" "; cout<<"\n";}
+int min(int a, int b) { if (a<b) return a; return b; }
+int max(int a, int b) { if (a>b) return a; return b; }
+std::string to_upper(std::string str) { std::transform(all(str), str.begin(), ::toupper); }
+std::string to_lower(std::string str) { std::transform(all(str), str.begin(), ::tolower); }
 
 #define ll long long
 // #define int long long
 
 void solve(){
+    int a,b,n,mod;
     
 }
 
