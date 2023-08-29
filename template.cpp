63cff6bb383249c0f14df41fb428d4aa0dce5d6f
Vivek Yanamadula <vivekyanamadula@gmail.com>
2023-08-29 00:25:33 -0400

    Adding my base template

---
+++ template.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define read(type) readInt<type>()
+
+#define pb push_back
+#define mp make_pair
+#define mpi pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+#define len(x) (int((x).size()))
+
+#define ll long long
+// #define int long long
+
+void solve() {
+    
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int tc; 
+    cin >> tc;
+    while(tc--) {
+        solve();
+    }
+    return 0;
+}
