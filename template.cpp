ff427e7e66b109c614a447ed4fe7dda51dc49876
Vivek Yanamadula <vivekyanamadula@gmail.com>
2023-09-20 10:51:49 -0400

    Modified templates with lecture on C++ data structures

--- template.cpp
+++ template.cpp
@@ -1,29 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define read(type) readInt<type>() //Faster read
+#define read(type) readInt<type>()
 
-#define pb push_back
 #define mp make_pair
-#define pii pair<int, int>
-#define a first
-#define b second
-#define vi vector<int>
 #define all(x) (x).begin(), (x).end()
-#define umap unordered_map
-#define uset unordered_set
 #define imax INT_MAX
 #define imin INT_MIN
 #define len(x) (int((x).size()))
-#define si set<int>;
-#define mii map<int, int>;
+
+#define umap unordered_map
+#define uset unordered_set
+#define pii pair<int, int>
+#define si set<int>
+#define useti unordered_set<int>
+#define mii map<int, int>
+#define dqi deque<int>
+#define pqi priority_queue<int>
 
 #define debug(x) cout << #x << " = " << x << "\n"; 
-
 #define printv(a) {for(auto u:a) cout<<u<<" "; cout<<"\n";}
+
 int min(int a, int b) { if (a<b) return a; return b; }
 int max(int a, int b) { if (a>b) return a; return b; }
-std::string to_upper(std::string str) { std::transform(all(str), str.begin(), ::toupper); }
-std::string to_lower(std::string str) { std::transform(all(str), str.begin(), ::tolower); }
+void to_upper(std::string str) { std::transform(all(str), str.begin(), ::toupper); }
+void to_lower(std::string str) { std::transform(all(str), str.begin(), ::tolower); }
 
 #define ll long long
 // #define int long long
