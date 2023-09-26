2d06c138942bd80ef901fa4dd80200663d6edd8e
Vivek Yanamadula <vivekyanamadula@gmail.com>
2023-09-25 20:46:24 -0400

    Changes to make template more based on ll instead of int

--- template_one_case.cpp
+++ template_one_case.cpp
@@ -2,33 +2,41 @@
 using namespace std;
 #define read(type) readInt<type>()
 
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
+#define ll long long
+
+#define llmax LLONG_MAX
+#define llmin LLONG_MIN
 #define imax INT_MAX
 #define imin INT_MIN
-#define len(x) (int((x).size()))
+
+#define mp make_pair
+#define f first
+#define s second
 
 #define umap unordered_map
 #define uset unordered_set
-#define pii pair<int, int>
-#define si set<int>
-#define usi unordered_set<int>
-#define mii map<int, int>
-#define dqi deque<int>
-#define pqi priority_queue<int>
+
+#define pll pair<ll, ll>
+#define sll set<ll>
+#define usll unordered_set<ll>
+#define mpll map<ll, ll>
+#define dqll deque<ll>
+#define pqll priority_queue<ll>
+
+#define all(x) (x).begin(), (x).end()
+#define len(x) (int((x).size()))
+#define fori(n) for(ll i = 0; i < n ; i++)
 
 #define debug(x) cout << #x << " = " << x << "\n"; 
 #define printv(a) {for(auto u:a) cout<<u<<" "; cout<<"\n";}
 
-int min(int a, int b) { if (a<b) return a; return b; }
-int max(int a, int b) { if (a>b) return a; return b; }
+ll min(ll a, ll b) { if (a<b) return a; return b; }
+ll max(ll a, ll b) { if (a>b) return a; return b; }
 void to_upper(std::string str) { std::transform(all(str), str.begin(), ::toupper); }
 void to_lower(std::string str) { std::transform(all(str), str.begin(), ::tolower); }
 
-#define ll long long
-// #define int long long
 
-void solve(int in){
+void solve(ll in){
     
 
 }
@@ -37,7 +45,7 @@ int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int in;
+    ll in;
     cin >> in;
     solve(in);
 
